9f64172e65c159f985bc274a7e95b01041deb899
leidianwudi <1234>
2020-09-12 11:51:12 +0800

    11

--- static/app/config.js
+++ static/app/config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  baseUrl: "http://18.182.50.223:9000",
-  version: "1.0.0",
+  baseUrl: "http://www.dz16666.com:9000",
+  version: "1.0.10",
   //http://localhost:8010  http://120.78.217.149:8027
-  //http://18.182.50.223:9000  聚赞
+  
+  //聚赞 后置
+  //http://18.182.50.223:9000
+  
+  //聚赞 前置
+  //http://www.dz16666.com:9000   最后版本1.0.10
 };
